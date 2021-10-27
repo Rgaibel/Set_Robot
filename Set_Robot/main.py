d9afd4a2ed245fd060bdfd8ad2b2697874b0741e
Rgaibel <rgaibel@gmail.com>
2021-10-27 14:26:08 +0300

    added the Python OpenSource code for the UArm Swift Pro. I initialized the and connected the robotic arm, and checked that the API works with basic movements.

--- Set_Robot/main.py
+++ Set_Robot/main.py
@@ -1,4 +1,6 @@
 import enum
+import time
+from uarm.wrapper import SwiftAPI
 
 
 # compares a single attribute of three given cards, to check generically, if they are all different, or all the same
@@ -29,6 +31,17 @@ def set_algo_for_loop(cards):
             for card3 in cards[2:9]:
                 if match(card1, card2, card3):
                     return card1.name, card2.name, card3.name
+    return "no matches, sorry"
+
+
+
+def set_algo_dynamic_programming(cards):
+    for card1 in cards[0:7]:
+        for card2 in cards[1:8]:
+            for card3 in cards[2:9]:
+                if match(card1, card2, card3):
+                    return card1.name, card2.name, card3.name
+    return "no matches, sorry"
 
 
 # all of out enums
@@ -80,3 +93,51 @@ if __name__ == '__main__':
     cards = [card1, card2, card3, card4, card5, card6, card7, card8, card9]
 
     print(set_algo_for_loop(cards))
+
+    accessed = False
+    # swift = 0
+    # while not accessed:
+#     try:
+    swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'}, callback_thread_pool_size=1)
+    swift.waiting_ready()
+        #     accessed = True
+        # except Exception as e:
+        #     time.sleep(0.2)
+        #     print('nothing')
+    swift.connect()
+    print('device info: ')
+    print(swift.get_device_info())
+
+    # swift.set_position(x=250, y=0, z=50, speed=100000)
+
+    speed = 100000
+    while swift.connected:
+        swift.set_position(x=300, y=0, z=150, speed=speed)
+        swift.set_position(z=50)
+        swift.set_position(z=150)
+        swift.set_position(x=200, y=100, z=100)
+        swift.set_position(z=50)
+        swift.set_position(z=150)
+        swift.set_position(x=200, y=0, z=150)
+        swift.set_position(z=50)
+        swift.set_position(z=150)
+
+        swift.set_polar(stretch=200, rotation=90, height=150, speed=speed)
+        swift.set_polar(stretch=200, rotation=45, height=150, speed=speed)
+        swift.set_polar(stretch=200, rotation=135, height=150, speed=speed)
+
+        swift.set_polar(stretch=200, rotation=135, height=90, speed=speed)
+        swift.set_polar(stretch=200, rotation=135, height=200, speed=speed)
+        swift.set_polar(stretch=200, rotation=135, height=150, speed=speed)
+
+        swift.set_polar(stretch=200, rotation=135, height=150, speed=speed)
+        swift.set_polar(stretch=250, rotation=135, height=150, speed=speed)
+
+        swift.set_servo_angle(0, 45, speed=speed)
+        swift.set_servo_angle(0, 135, speed=speed)
+        swift.set_servo_angle(1, 45, speed=speed)
+        swift.set_servo_angle(1, 90)
+        swift.set_servo_angle(2, 45)
+        swift.set_servo_angle(2, 60)
+        swift.set_servo_angle(3, 45)
+        swift.set_servo_angle(3, 135)
